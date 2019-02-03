516704b851e6cb3c86dd25bdc237d23cfec1cc2e
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-02-03 18:33:15 +0200

    Add files via upload

---
+++ HW0302/HW0302/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW0302
+{
+    class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+        public Game()
+        {
+
+        }
+
+        public Game(string name, float rating = 5.5f)
+        {
+            _name = name;
+            _rating = rating;
+        }
+
+        public Game(string name, int numberOfPlayers, float rating, string originCountry)
+        {
+            _name = name;
+            _numberOfPlayers = numberOfPlayers;
+            _originCountry = originCountry;
+            _rating = rating;
+        }
+
+        public Game(string name, string originCountry)
+        {
+            _name = name;
+            _originCountry = originCountry;
+        }
+
+        public int TellMeHowManyPlayers()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return $"Game Name : {_name} Number of players : {_numberOfPlayers} Rating : {_rating} Origin country : {_originCountry}";
+        }
+    }
+
+}
---
+++ HW0302/HW0302/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW0302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game chess = new Game("Chess", 2, 9.5f, "Persia");
+            Game soccer = new Game("Soccer")
+            {
+                _originCountry = "Brazil"
+            };
+            soccer._numberOfPlayers = 22;
+
+        }
+
+        static void PrintGame(Game game)
+        {
+            Console.WriteLine(game);
+            int number = game.TellMeHowManyPlayers();
+        }
+    }
+}
