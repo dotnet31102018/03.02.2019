7fe91007f981da6a991f806eae33759edc2080b0
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-02-03 18:33:56 +0200

    Add files via upload
    Running Console application with arguments

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arguments:");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No Arguments!");
+                return;
+            }
+            if (args.Length == 1)
+            {
+                Console.WriteLine("Need two Arguments!");
+                return;
+            }
+
+            
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine($"{args[0]} + {args[1]} = { Convert.ToInt32(args[0]) + Convert.ToInt32(args[1])}");
+        }
+    }
+}
